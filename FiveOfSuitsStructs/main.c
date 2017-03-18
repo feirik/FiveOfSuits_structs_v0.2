e7267db130b516c501cb46c3df19139ae2b6eae4
feirik <feirik@gmail.com>
2017-03-18 12:23:12 +0100

    Initial Commit

---
+++ FiveOfSuitsStructs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FiveOfSuitsStructs
+//
+//  Created by Eirik F on 18.03.17.
+//  Copyright (c) 2017 Eirik F. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
